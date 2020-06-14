bf72f7a94f3f6df69b24bd3fc6f622a44af718ef
Ken Charette <charettekenneth@gmail.com>
2020-06-14 00:06:40 -0400

    Updated font and styles

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import CurrencyRow from './components/CurrencyRow'
 
 const App = () => {
   return (
-    <>
-      
-      <div className='flex flex-col justify-center items-center text-center min-h-screen text-blue-500'>
-        <h1 className=''>Currency Conversion </h1>
-        <CurrencyRow />
-        <div>=</div>
-        <CurrencyRow />
+    <>  
+      <div className='flex flex-col justify-center items-center text-center min-h-screen text-gray-900'>
+        <div className='border border-red-500 p-20 bg-gray-100 rounded-lg'>
+          <h1 className=''>Currency Conversion </h1>
+            <div className='flex flex-wrap flex-row m-2 border border-orange-500'>
+              <CurrencyRow />
+              <div className='m-2 font-bold'>=</div>
+              <CurrencyRow />
+            </div>
+            <h2 className='text-gray-900 text-left border border-blue-500'>Exchange Rate</h2>
+        </div>
       </div>
     </>
   );
--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 export default function CurrencyRow() {
     return (
+        <>
         <div>
-            <input type='number' />
-            <select>
-                <option value='Hi'> Hi </option>
-            </select>
+            <div className='border-2 border-green-500 m-2'>
+                <input type='number' />
+                <select className='border border-green-500'>
+                    <option value='Hi'> Hi </option>
+                </select>
+            </div>
         </div>
+        </>
     )
 }
